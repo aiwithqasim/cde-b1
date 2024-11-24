b380de6ef17a624adc9ac051f81454b98d9c6303
aiwithqasim <qasimhassan1020@gmail.com>
2024-11-24 16:28:10 +0500

    class 02 & 03

---
+++ class02/experiment.py
@@ -0,0 +1,9 @@
+# Data Types: INTEGER, STRING, FLOAT, BOOLEAN
+name = "Qasim" # String
+age = 20 # Integer
+weight = 20.5 #Float
+is_present = True #Boolean
+
+# TAB is used to guess & auto complete
+# printing more then one vaiavle convert it into TUPLE(we'll cover it later)
+print(type(name), type(age), type(weight), type(is_present))
